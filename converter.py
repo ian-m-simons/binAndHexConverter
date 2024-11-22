38fb241ad5f88f5b3fc60259977c3922b9a5fbff
Ian Simons <iansimons93@gmail.com>
2024-11-22 11:44:28 -0500

    built skeleton

---
+++ converter.py
@@ -0,0 +1,37 @@
+
+
+#features
+def binToDec():
+    continue
+
+def decToBin():
+    continue
+
+def hexToDec():
+    continue
+
+def decToHex():
+    continue
+
+def binToHex():
+    continue
+
+def hexToBin():
+    continue
+
+#input verification functions
+def inputInt():
+    continue
+
+def verifyDec():
+    continue
+
+def verifyHex():
+    continue
+
+def verifyBin():
+    continue
+
+#mainMenu
+def main():
+    #create menu
