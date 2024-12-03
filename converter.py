b0af65a976f835a339b2ed4f208c22b1c0c33474
Ian Simons <iansimons93@gmail.com>
2024-12-03 08:47:44 -0500

    adding hex to bin

--- converter.py
+++ converter.py
@@ -124,8 +124,12 @@ def binToHex(binNum):
 
     
 
-def hexToBin():
-    return 0
+def hexToBin(hexNum):
+    table = {"0":"0000", "1":"0001", "2":"0010", "3":"0011", "4":"0100", "5":"0101", "6":"0110", "7":"0111", "8":"1000", "9":"1001", "A":"1010", "B":"1011", "C":"1100", "D":"1101", "E":"1110", "F":"1111"}
+    solution = ""
+    for i in range(len(hexNum)):
+        solution += table[hexNum[i]]
+    return solution
 
 #input verification functions
 def inputInt(prompt):
@@ -231,8 +235,7 @@ def main():
                 print("[ERROR] input may only contain 0-9 and A-F")
                 num = input("please enter a hexadecimal number: ")
                 correctHex = verifyHex(num)
-            num = hexToDec(num)
-            num = decToBin(num)
+            num = hexToBin(num)
             print(num)
         elif choice == 0:
             exit(0)
